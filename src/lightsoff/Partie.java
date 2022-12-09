c1bde2026ec3488eea2fbdc202a87056e9242227
alexia <alexia@DESKTOP-3LIGDG0.education.lakanal.epf.fr>
2022-12-09 12:01:19 +0100

    classe partie

--- src/lightsoff/Partie.java
+++ src/lightsoff/Partie.java
@@ -4,6 +4,9 @@
  */
 package lightsoff;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author alexia
@@ -11,4 +14,34 @@ package lightsoff;
 public class Partie {
     private Joueur joueurcourant;
     private Grille plateau;
+    
+
+    public void allumer_lumieres(int l, int c){
+        Random nbr = new Random();
+        int nb_case = nbr.nextInt(5);
+        
+        for (int i=0; i<5; i++ ){
+            Random n=new Random();
+            int ligne = n.nextInt(2);
+            Random nb = new Random();
+            
+        }
+        for (int k=0; k<nb_case; k++){
+            plateau.AllumerCase(l,c);
+        }   
+    }
+    
+    public void lancer_partie(){
+        
+        while (plateau.EtreGagnant()==false){
+
+            System.out.println("Choisissez la cellule à allumer" + '\n' + "Exprimer la coordonnée en Ligne puis Colonne");
+            Scanner sc = new Scanner(System.in);
+            int ligneCellule = sc.nextInt();
+            int colCellule = sc.nextInt();
+            plateau.clic_sur_case(ligneCellule, colCellule); 
+            
+        }   
+    }
+    
 }
