cb9107d93e8c6be59bed1c358019eb53e3d18a71
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-12-09 10:01:38 +0100

    interface graphique

--- src/lightsoff/Grille.java
+++ src/lightsoff/Grille.java
@@ -27,7 +27,7 @@ public class Grille { // on initialise notre grille de jeu
         int compteur = 0;
         for (int i=0; i<5; i++){
             for (int j=0; j<5; j++){
-                if ( GrilleDeJeu[i][j].etreEteinte() ==true ){
+                if ( GrilleDeJeu[i][j].etreEteinte() == true ){
                     compteur +=1;
                 }
             }
@@ -40,31 +40,39 @@ public class Grille { // on initialise notre grille de jeu
         }
     }
     
-    public void clic_sur_case(int x, int y){
+    public void clic_sur_case(int x, int y){ //on change l'état de la cellule cliqué et des 4 alentours   
+        GrilleDeJeu[x][y].changerCellule();
+        GrilleDeJeu[x+1][y].changerCellule();
+        GrilleDeJeu[x-1][y].changerCellule();
+        GrilleDeJeu[x][y+1].changerCellule();
+        GrilleDeJeu[x][y-1].changerCellule();
+        
         
         
     }
     
     public void AllumerCase (int x, int y){ 
-        if (GrilledeJeu[x][y].etreAllumee()==false ){
-            GrilledeJeu[x][y].changerCellule();
+        if (GrilleDeJeu[x][y].etreAllumee()==false ){
+            GrilleDeJeu[x][y].changerCellule();
         }
         
     }
     
     public void EteindreCase (int x, int y){
-         if (GrilledeJeu[x][y].etreAllumee()==true ){
-            GrilledeJeu[x][y].changerCellule();
+         if (GrilleDeJeu[x][y].etreAllumee()==true ){
+            GrilleDeJeu[x][y].changerCellule();
         }
         
         
     }
     
-    public void CaseEteinte (int X, int y){
+    public void CaseEteinte (int x, int y){
+        GrilleDeJeu[x][y].etreEteinte();
         
     }
     
     public void CaseAllume (int x, int y){
+        GrilleDeJeu[x][y].etreAllumee();
         
     }
     
