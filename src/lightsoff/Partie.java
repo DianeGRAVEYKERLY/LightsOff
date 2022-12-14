afbb48fe04d80d3268b3a8ac8216b635cb9d7b2a
alexia <alexia@DESKTOP-3LIGDG0.education.lakanal.epf.fr>
2022-12-14 08:46:49 +0100

    classe partie : allumer des cases aléatoires et lancer la partie

--- src/lightsoff/Partie.java
+++ src/lightsoff/Partie.java
@@ -16,27 +16,30 @@ public class Partie {
     private Grille plateau;
     
 
-    public void allumer_lumieres(int l, int c){
+    public void allumer_lumieres(){ //on allume quelques cases aléatoires 
         Random nbr = new Random();
-        int nb_case = nbr.nextInt(5);
+        int nb_case = nbr.nextInt(7);
         
-        for (int i=0; i<5; i++ ){
-            Random n=new Random();
-            int ligne = n.nextInt(2);
-            Random nb = new Random();
+        
+        for (int i=0; i<nb_case; i++ ){
             
-        }
-        for (int k=0; k<nb_case; k++){
-            plateau.AllumerCase(l,c);
+            Random n=new Random();
+            int ligne = n.nextInt(5);
+            Random nb = new Random();
+            int colonne = nb.nextInt(6);
+            plateau.AllumerCase(ligne,colonne);
+
         }   
     }
     
-    public void lancer_partie(){
+    public void lancer_partie(){ //tant qu'il y a des lumières allumées on demande au joueur de choisir une ligne et colonne de la cellule qu'il veut changer d'etat
+        
+        allumer_lumieres();
         
         while (plateau.EtreGagnant()==false){
 
-            System.out.println("Choisissez la cellule à allumer" + '\n' + "Exprimer la coordonnée en Ligne puis Colonne");
-            Scanner sc = new Scanner(System.in);
+            System.out.println("Choisissez la cellule à éteindre" + '\n' + "Exprimer la coordonnée en Ligne puis Colonne");
+            Scanner sc = new Scanner(System.in); 
             int ligneCellule = sc.nextInt();
             int colCellule = sc.nextInt();
             plateau.clic_sur_case(ligneCellule, colCellule); 
