4268169f082cea6724c2ce88fd18c6e7d34eb8dc
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-12-14 09:18:40 +0100

    interface graphique et cellule graphique, import des images et affichage des images en fonction de si la cellule est allumée ou pas

--- src/lightsoff/interfaceGraphique.java
+++ src/lightsoff/interfaceGraphique.java
@@ -1,5 +1,8 @@
 package lightsoff;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -45,17 +48,14 @@ public class interfaceGraphique extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panneau_grille, javax.swing.GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(panneau_grille, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 492, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(panneau_grille, javax.swing.GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(panneau_grille, javax.swing.GroupLayout.DEFAULT_SIZE, 453, Short.MAX_VALUE)
+                .addGap(33, 33, 33))
         );
 
         pack();
@@ -94,6 +94,38 @@ public class interfaceGraphique extends javax.swing.JFrame {
                 new interfaceGraphique().setVisible(true);
             }
         });
+        
+    }
+         public void lancer_partie(){ //tant qu'il y a des lumières allumées on demande au joueur de choisir une ligne et colonne de la cellule qu'il veut changer d'etat
+        
+        allumer_lumieres();
+        
+        while (plateau.EtreGagnant()==false){
+
+            System.out.println("Choisissez la cellule à éteindre" + '\n' + "Exprimer la coordonnée en Ligne puis Colonne");
+            Scanner sc = new Scanner(System.in); 
+            int ligneCellule = sc.nextInt();
+            int colCellule = sc.nextInt();
+            plateau.clic_sur_case(ligneCellule, colCellule); 
+            
+        }   
+    }
+    
+    
+public void allumer_lumieres(){ //on allume quelques cases aléatoires 
+        Random nbr = new Random();
+        int nb_case = nbr.nextInt(7);
+        
+        
+        for (int i=0; i<nb_case; i++ ){
+            
+            Random n=new Random();
+            int ligne = n.nextInt(5);
+            Random nb = new Random();
+            int colonne = nb.nextInt(6);
+            plateau.AllumerCase(ligne,colonne);
+
+        }   
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
